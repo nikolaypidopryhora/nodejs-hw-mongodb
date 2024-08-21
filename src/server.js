f9ed3e2ae8aa5868fc051896eb9c116e1fcfd042
Niko Pidopryhora <nikolaypidopryhora@gmail.com>
2024-08-21 15:47:41 +0200

    some fixes

--- src/server.js
+++ src/server.js
@@ -34,13 +34,6 @@ export const setupServer = () => {
 
   app.use(errorHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
